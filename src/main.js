18779fedee4c68f9be8f9d2bb8f5df0354096eab
Tochka <tochka114@gmail.com>
2024-06-01 18:11:19 +0300

    1

---
+++ src/main.js
@@ -0,0 +1,5 @@
+
+import "simplelightbox/dist/simple-lightbox.min.css";
+import "./js/1-gallery"
+import SimpleLightbox from "simplelightbox";
+import SimpleLightbox from "simplelightbox/dist/simple-lightbox.esm";
